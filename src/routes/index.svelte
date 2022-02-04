38983595141ccbc80db68e8bd0a9063806fb62cb
Mt-Lampert <perrin.blog@web.de>
2022-02-04 10:37:40 +0100

    p: updating the downloaded header

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,31 +2,18 @@
 	export const prerender = true;
 </script>
 
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
+
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+	<h1 class="has-text-info-dark">Hello, Folks</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<p class="has-text-grey">This is SvelteKit with <span class="has-text-primary">Bulma</span></p>
 
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<p>And it works!!!!</p>
 </section>
 
 <style>
@@ -42,18 +29,5 @@
 		width: 100%;
 	}
 
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+	
 </style>
