cf415180a6511aaf872d433e3dbfc3eb50502b58
Mt-Lampert <perrin.blog@web.de>
2022-02-07 09:21:17 +0100

    w: implementing a pretty under construction feature

---
+++ src/lib/UnderConstruction.svelte
@@ -0,0 +1,9 @@
+<div class="under-construction">
+    <img src="/images/under-construction.png" alt="Under Construction!" />
+</div>
+
+<style>
+  .under-construction {
+    margin-bottom: 2rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,15 @@
 <script context="module">
+	import UnderConstruction from "$lib/UnderConstruction.svelte"
 	export const prerender = true;
 </script>
-
-
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <section>
+	<UnderConstruction />
+
 	<h1 class="has-text-info-dark">Hello, Folks</h1>
 
 	<p class="has-text-grey">This is SvelteKit with <span class="has-text-primary">Bulma</span></p>
@@ -28,6 +29,8 @@
 	h1 {
 		width: 100%;
 	}
-
 	
+	.under-construction {
+		margin-bottom: 2rem;
+	}
 </style>
