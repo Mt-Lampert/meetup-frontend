9a1ba04129d97e9837b2c1207b80bb208da6ee1c
Mt-Lampert <perrin.blog@web.de>
2022-02-10 16:55:35 +0100

    w: adding gallery card component

---
+++ src/lib/GalleryCard.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let meetup = {}
+</script>
+
+<div class="card column is-one-third">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img src="/images/{meetup.img}" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <h1 class="title is-4">{meetup.title}</h1>
+    <h2 class="subtitle">{meetup.city}, {meetup.date}</h2>
+  </div>
+</div>
+
+<style>
+  h2 {
+    text-align: center;
+  }
+</style>
